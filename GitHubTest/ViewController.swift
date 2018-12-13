039defd46a22d48ffd99dadae1a2d4c75bc30c15
Pulkit <pulkit.kaushik@vvntech.in>
2018-12-13 15:07:21 +0530

    PRJCBUGEN-909090 : Background Color of view changed

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -11,12 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setUIAndData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    /// Set UI and Data
+    func setUIAndData() {
+        view.backgroundColor = UIColor.lightGray
     }
 
 
